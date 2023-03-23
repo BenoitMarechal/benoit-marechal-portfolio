057b30ae6a0e1f54b65d2eacd21d83dfc961b099
BenoitMarechal <benoit.marechal.56@gmail.com>
2023-03-23 18:10:01 +0100

    ajout liens

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import Footer from '../../components/footer/Footer';
 import Header from '../../components/Header/Header';
-import { Link } from 'react-router-dom';
 const Projects = () => {
   return (
     <div className='app'>
       <Header></Header>
       <main>
         <h1>PROJETS</h1>
-        <Link
-          to={{ pathname: 'https://benoitmarechal.github.io/Livrable2/' }}
-          target='_blank'
-        >
-          RESERVIA
-        </Link>
+
         <a
           href='https://benoitmarechal.github.io/Livrable2/'
           target='_blank'
