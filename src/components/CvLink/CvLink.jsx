77fab46d5063b1bc4ff62f8a3aff4f28fef3522f
BenoitMarechal <benoit.marechal.56@gmail.com>
2023-04-04 13:14:55 +0200

    lien download ok

--- src/components/CvLink/CvLink.jsx
+++ src/components/CvLink/CvLink.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import pdf from '../../assets/documents/CV_BENOIT_MARECHAL.pdf';
+import pdfLogo from '../../assets/logos/pdfLogo.png';
 
 const CvLink = () => {
   return (
-    <div>
-      <a href={pdf} target='_blank' rel='noopener noreferrer'>
+    <div className='cv__downloadLink'>
+      <img
+        src={pdfLogo}
+        alt='Version pdf du CV'
+        className='cv__downloadLink__logo'
+      />
+      <a
+        href={pdf}
+        className='cv__downloadLink__link'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
         Accéder au pdf
       </a>
     </div>
