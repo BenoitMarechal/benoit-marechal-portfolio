180604a3510bfe72771c9ac4d8f1fa4531753b74
BenoitMarechal <benoit.marechal.56@gmail.com>
2023-04-11 17:31:09 +0200

    import overview ok

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import projectLabels from '../../assets/projectLabels.json';
 import { Link } from 'react-router-dom';
+//import logo from '../../../public/projectImages/argentbank/overview.png';
 
 const ProjectCard = (props) => {
-  //console.log(props);
+  let folderName = props.name.toLowerCase().replace(/\s/g, '');
+  let overViewPath = './projectImages/' + folderName + '/overview.png';
+
+  // useEffect(() => {
+  //   const folderName = props.name.toLowerCase().replace(/\s/g, '');
+  //   const overViewPath =
+  //     '../../assets/photos/projets/' + folderName + '/overview.png';
+  //   import(overViewPath)
+  //     .then((imageModule) => {
+  //       setOverView(imageModule.default);
+  //     })
+  //     .catch((error) => {
+  //       console.error('Failed to load image:', error);
+  //     });
+  // }, [props.name]);
   return (
     <div
       className={props.visible === 'true' ? 'propsCard ' : ' propsCard  none'}
@@ -12,6 +27,7 @@ const ProjectCard = (props) => {
       <Link to={'/project/?name=' + props.name} {...props}>
         <h2 className='projectCard__h2'>{props.name}</h2>
       </Link>
+      <img src={overViewPath} alt={'logo'} />
 
       {props.links
         ? props.links.map((link, index) => (
