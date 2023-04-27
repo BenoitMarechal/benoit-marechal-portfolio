5bda8a67b4239153df48d57407f934cd2e7ffb62
BenoitMarechal <benoit.marechal.56@gmail.com>
2023-04-27 14:08:57 +0200

    finition footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,39 @@
 import React from 'react';
+import { BsLinkedin, BsGithub } from 'react-icons/bs';
 
 const Footer = () => {
   return (
     <footer className='footer bg-gradient'>
-      <p> &copy; Benoit Maréchal, 2023</p>
-      <p>Développé sur React</p>
-      <p>Hébergé sur GitHub </p>
-      <p>Déployé sur Vercel</p>
+      <div className='footer__container'>
+        <p className='footer__p'>
+          {' '}
+          Benoit Maréchal, 2023{' '}
+          <a
+            className='footer__p__a'
+            href='https://www.linkedin.com/in/benoit-mar%C3%A9chal-149ab3207/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <BsLinkedin />
+          </a>
+        </p>
+        <p className='footer__p'>
+          Hébergé sur GitHub{' '}
+          <a
+            className='footer__p__a'
+            href='https://github.com/BenoitMarechal/benoit-marechal-portfolio.git'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <BsGithub />{' '}
+          </a>
+        </p>
+      </div>
+      {/* <div className='footer__container'>
+        <p className='footer__p'>Développé sur React</p>
+
+        <p className='footer__p'>Déployé sur Vercel</p>
+      </div> */}
     </footer>
   );
 };
